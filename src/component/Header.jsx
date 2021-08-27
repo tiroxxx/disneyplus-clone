5ad9dcb4ed6e3f66d7fa3c0f78bed241c8cd5668
Christian Marquez <christianmarquez41@gmail.com>
2021-08-27 15:35:22 -0400

    adding styling to nav

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,27 +1,43 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 function Header() {
-    return (
-        <Nav>
-           <Logo src="/images/logo.svg" />
-           <NavMenu>
-
-           </NavMenu>
-        </Nav>
-    )
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
 }
 
-export default Header
+export default Header;
 
 const Nav = styled.nav`
-    height: 70px;
-    background: #090b13
-`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
 
 const Logo = styled.img`
-    width: 80px;
-`
+  width: 80px;
+`;
 
 const NavMenu = styled.div`
+  display: flex;
 
-`
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
+    img {
+        height: 20px;
+    }
+  }
+`;
