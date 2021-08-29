01de21668effe76f9024b0ff9bf75bc92480eb81
Christian Marquez <christianmarquez41@gmail.com>
2021-08-29 15:36:53 -0400

    added movie slice and movies to the store

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,14 +4,19 @@ import ImageSlider from './ImageSlider';
 import Categories from './Categories';
 import Movies from './Movies';
 import db from '../firebase';
+import { useDispatch } from 'react-redux';
+import { setMovies } from '../features/movie/movieSlice';
 
 function Home() {
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    db.collection('movies').onSnapshot((snapshot) => {
-      const tempMovies = snapshot.docs.map((doc) => {
-        return { id: doc.id, ...doc.data() };
-      });
-    });
+    // db.collection('movies').onSnapshot((snapshot) => {
+    //   const tempMovies = snapshot.docs.map((doc) => {
+    //     return { id: doc.id, ...doc.data() };
+    //   });
+    //   dispatch(setMovies(tempMovies));
+    // });
   });
 
   return (
