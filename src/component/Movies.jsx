d7dd9165c5a5452db1a0c0dc23a79c213d89eefa
Christian Marquez <christianmarquez41@gmail.com>
2021-08-29 19:01:41 -0400

    displayed movie info when taken to the detail page of the movie

--- src/component/Movies.jsx
+++ src/component/Movies.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { selectMovies } from '../features/movie/movieSlice';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function Movies() {
   const movies = useSelector(selectMovies);
@@ -11,32 +12,12 @@ function Movies() {
       <Content>
         {movies &&
           movies.map((movie, idx) => (
-            <Wrap>
-              <img
-                src={movie.cardImg}
-                alt=""
-              />
+            <Wrap key={idx}>
+              <Link to={`/detail/${movie.id}`}>
+                <img src={movie.cardImg} alt="" />
+              </Link>
             </Wrap>
           ))}
-
-        <Wrap>
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt=""
-          />
-        </Wrap>
-        <Wrap>
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt=""
-          />
-        </Wrap>
-        <Wrap>
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt=""
-          />
-        </Wrap>
       </Content>
     </Container>
   );
